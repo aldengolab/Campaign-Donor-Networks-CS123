0a70676ab49471a969155a5ee3b7685e33cff9dd
mackp <mackp@uchicago.edu>
2016-05-28 18:24:38 -0500

    testing with top pac  contributors

--- explore_scripts/explore_1.py
+++ explore_scripts/explore_1.py
@@ -3,41 +3,46 @@
 '''
 from mrjob.job import MRJob
 import heapq
+import csv
 
 
+ID = 0
 AMOUNT = 8
 CONTRIBUTOR_NAME = 10
+SEAT = 36
 K = 50
+DONOR_TYPE = 12
 
 
 class total_donations(MRJob):
     def fields(self,line):
         '''Contructs Fields from CSV File'''
-        fields = line.split(",")
-        #ignore header
         try:
-            amount = float(fields[AMOUNT])
-            donor = fields[CONTRIBUTOR_NAME]
-        except:
+            rdr = csv.reader([line])
+            columns = rdr.next()
+            for i in range(len(columns)):
+                columns[i] = columns[i].strip("'\"\/").lower()
+            #ignore header
+            if columns[0] != 'id':
+                amount = float(columns[AMOUNT])
+                donor = columns[CONTRIBUTOR_NAME].lower()
+                donor_type = columns[DONOR_TYPE]
+            if columns[0] == 'id':
+                amount = None
+                donor = None
+                donor_type = None
+        except Exception as e:
             amount = None
             donor = None
-   
-        # if  fields[AMOUNT] == 'amount':
-        #     amount = None
-        # else:
-        #     amount = float(fields[AMOUNT])
-        # if fields[CONTRIBUTOR_NAME] == "contributor_name":
-        #     donor = None
-        # else:
-        #     donor = fields[CONTRIBUTOR_NAME]
+            donor_type =  None
 
-        return donor, amount
+        return donor, amount, donor_type
 
     def mapper(self, _, line):
         '''Yields the Name on Each line  and the value 1'''
-        donor, amount = self.fields(line)
+        donor, amount, donor_type = self.fields(line)
         if donor and amount:
-            yield donor, amount
+            yield (donor_type,donor),amount
 
     def combiner(self, donor, amount):
         ''' Combines names '''
@@ -49,14 +54,19 @@ class total_donations(MRJob):
         heapq.heapify(h)
         self.h = h
 
+
+
     def reducer(self, donor, amount):
         ''' replaces min val of the heap if the amount number is greater than the min value '''
-        donor = str(donor)
-        amount = sum(amount)
-        replacement = (amount, donor)
-        if replacement > min(self.h):
-            if donor:
-                heapq.heapreplace(self.h,replacement)
+        if donor[0] == 'c':
+            donor = str(donor[1])
+            amount = sum(amount)
+            replacement = (amount, donor)
+            if replacement > min(self.h):
+                if donor:
+                    heapq.heapreplace(self.h,replacement)
+
+
 
     def reducer_final(self):
         ''' reverse sorts the heap and yields the tuple containing the number of visits and names of the Top K '''
@@ -64,6 +74,7 @@ class total_donations(MRJob):
         top.sort(reverse = True)
         for i in top:
             yield i
+
 
 
 if __name__ == '__main__':
