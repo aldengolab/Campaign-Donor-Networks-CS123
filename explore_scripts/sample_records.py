24353ccb2bfa41c06ba473fb4e193bcad3fa6c5d
Alden Golab <agolab@wireless-s1-so-150-60-141.uchicago.edu>
2016-05-29 18:23:13 -0500

    Fixed sampling issue

--- explore_scripts/sample_records.py
+++ explore_scripts/sample_records.py
@@ -1,5 +1,6 @@
 
 from mrjob.job import MRJob
+from mrjob.protocol import ReprValueProtocol
 import random
 
 
@@ -7,13 +8,15 @@ class sample_records(MRJob):
     '''
     Sample records to N from full sample.
     '''
+    OUTPUT_PROTOCOL = ReprValueProtocol
+    
     def mapper(self, _, line):
         fields = line.split()
         if  fields[0] == 'id':
             yield line,  None
         rand = random.randrange(1,100)
         if rand == 1:
-            yield line, None
+            yield None, line
 
 if __name__ == '__main__':
     sample_records.run()
