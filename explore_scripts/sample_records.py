f6e1e197c1cc4be4685441c2bde40547a7958d4a
mackp <mackp@uchicago.edu>
2016-05-29 18:14:34 -0500

    sampling

--- explore_scripts/sample_records.py
+++ explore_scripts/sample_records.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python
 
 from mrjob.job import MRJob
-import math
+import random
 
-N = 500000.0
 
 class sample_records(MRJob):
     '''
     Sample records to N from full sample.
     '''
     def mapper(self, _, line):
-        rand = random.uniform(0,1)
-        if rand <= (1.0/N):
-            yield line, ''
+        fields = line.split()
+        if  fields[0] == 'id':
+            yield line,  None
+        rand = random.randrange(1,100)
+        if rand == 1:
+            yield line, None
+
+if __name__ == '__main__':
+    sample_records.run()
