902952251be65aecac63eb06fe55e97bb1364489
Alden Golab <agolab@Aldens-MacBook-Pro.local>
2016-05-30 18:52:44 -0500

    Remove non-ascii characters from sample

--- explore_scripts/sample_records.py
+++ explore_scripts/sample_records.py
@@ -2,6 +2,7 @@
 from mrjob.job import MRJob
 from mrjob.protocol import ReprValueProtocol
 import random
+import string
 
 
 class sample_records(MRJob):
@@ -11,12 +12,14 @@ class sample_records(MRJob):
     OUTPUT_PROTOCOL = ReprValueProtocol
     
     def mapper(self, _, line):
+        line = filter(lambda x: x in string.printable, line)
         fields = line.split()
         if  fields[0] == 'id':
-            yield line,  None
-        rand = random.randrange(1,100)
-        if rand == 1:
             yield None, line
+        else:
+            rand = random.randrange(1,100)
+            if rand == 1:
+                yield None, line
 
 if __name__ == '__main__':
     sample_records.run()
