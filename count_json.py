1c10205a9e5bf1353ca451c8298b473a6099c342
mackp <mackp@uchicago.edu>
2016-06-01 17:30:41 -0500

    script to get summary statistics of dedupe json

---
+++ count_json.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import re
+import os
+import heapq
+
+
+K = 10
+
+def run(filename):
+    '''create statistics  on the json alias file'''
+    with open(filename, 'rU') as f:
+        aliases = json.load(f)
+
+    count = {}
+    vals = aliases.values()
+
+    for val in vals:
+        if val in count:
+            count[val] = count[val] +1
+        else:
+            count[val] = 1
+
+    h = [(0, " ")] * K
+    heapq.heapify(h)
+    for key, val in count.items():
+        replacement = (val, key) 
+        if replacement > min(h):
+            heapq.heapreplace(h,replacement)
+    print "There are", len(count.keys()),  "unique businesses represented in the json \n"
+    print "There are", len(aliases),  "aliases in the json \n"
+    print "The top 10 companies with the most aliases are: \n"
+    h = list(h)
+    h.sort(reverse=True)
+
+    for i in h:
+        print i[1], "had " , i[0], " aliases \n"
+
+
+if __name__ == "__main__":
+    assert '.json' in sys.argv[1]
+    run(sys.argv[1])
