45f9b9056e2518d513a56f39e5b77b90f3a0575e
Alden Golab <agolab@wireless-s1-so-150-49-177.uchicago.edu>
2016-06-02 11:51:36 -0500

    Fixed json correction code

--- entity_reduction_scripts/fix_json_output.py
+++ entity_reduction_scripts/fix_json_output.py
@@ -10,6 +10,7 @@ def transform_file(filename):
     jsons = []
     #found at:
     num_lines = sum(1 for line in open(filename))
+
     i = 0
     f = open(filename, 'rU')
     while i < num_lines:
@@ -18,12 +19,15 @@ def transform_file(filename):
         i+=1
     f.close()
     
-    first = jsons[0]
-    second = jsons[1]
-    for i in range(len(jsons) -1):
+    first = json.loads(jsons[0])
+    second = json.loads(jsons[1])
+    rv = merge(first, second)
+    for i in range(len(jsons)):
         if i != 0:
-            rv = merge(jsons[i], jsons[i + 1])
-    print rv
+            rv = merge(rv, json.loads(jsons[i]))
+            
+    with open('output.json', 'w') as f: 
+        json.dump(rv, f)
 
     
 
